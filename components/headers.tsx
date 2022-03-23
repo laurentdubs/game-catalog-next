9fc906cd1f9a07837ce079233fc56c6d85bae1c1
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-03-23 15:06:31 +0100

    add navbar

--- components/headers.tsx
+++ components/headers.tsx
@@ -1,40 +1,7 @@
 import React from "react";
-import Link from "next/link";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { useRouter } from "next/router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-// export const Header = () => {
-//   const router = useRouter();
-//   console.log(router);
-//   const styles = {
-//     header: {
-//       margin: 20,
-//       padding: 20,
-//       border: "1px solid #DDD",
-//     },
-//     link: {
-//       margin: 15,
-//     },
-//     active: {
-//       margin: 15,
-//       color: "blue"
-//     }
-//   };
-//   return (
-//     <div style={styles.header}>
-//       <Link href="/" passHref>
-//         <span style={router.pathname === "/" ? styles.active : styles.link}>Home</span>
-//       </Link>
-//       <Link href="/games/games" passHref>
-//         <span style={router.pathname === "/games/games" ? styles.active : styles.link}>Games</span>
-//       </Link>
-//       <Link href="/platforms/platforms" passHref>
-//         <span style={router.pathname === "/platforms/platforms" ? styles.active : styles.link}>Platforms</span>
-//       </Link>
-//     </div> 
-//   );
-// };
 
 export const Header = () => {
 
@@ -45,7 +12,7 @@ export const Header = () => {
                     <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/games">Games</Nav.Link>
-                        <Nav.Link href="/platforms">Platforms</Nav.Link>
+                        <Nav.Link href="platforms">Platforms</Nav.Link>
                     </Nav>
             </Container>
         </Navbar>
